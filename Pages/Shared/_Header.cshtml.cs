2a9d32763dde6abd26184bdb1eefbeb616f767f4
Shadugr <shadircomm@gmail.com>
2022-10-02 18:12:47 +0300

    Initial commit

---
+++ Pages/Shared/_Header.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Web;
+
+namespace FMI_web.Pages.Shared
+{
+    public class _HeaderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
