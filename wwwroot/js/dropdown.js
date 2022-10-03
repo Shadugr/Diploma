e3548a4e6a9f8d30b784170b9d826dffaccb1cba
Shadugr <shadircomm@gmail.com>
2022-10-03 20:53:40 +0300

    feat: scroll in nav and fixed bounding.

---
+++ wwwroot/js/dropdown.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    $('li.parent').on('mouseover', function () {
+        var $menuItem = $(this),
+            $submenuWrapper = $('> .wrapper', $menuItem);
+        
+        var menuItemPos = $menuItem.position();
+
+        $submenuWrapper.css({
+            top: menuItemPos.top,
+            left: menuItemPos.left + Math.round($menuItem.outerWidth())
+        });      
+    });
+});
---
+++ wwwroot/js/out_of_screen.js
@@ -0,0 +1,31 @@
+﻿window.addEventListener("resize", removeMirror);
+
+function giveMirror() {
+	var elem = document.getElementsByClassName("parent");
+	
+	for (var i = 0; i < elem.length; i++) {
+		var child = elem[i].getElementsByClassName("wrapper");
+		var isOut = isOutOfViewport(child[0]);
+		if (isOut.right) {
+			child[0].classList.add("mirror");
+        }
+    }
+}
+
+function removeMirror() {
+	var elem = document.getElementsByClassName("wrapper");
+	for (var i = 0; i < elem.length; i++) {
+		elem[i].classList.remove("mirror");
+    }
+}
+
+var isOutOfViewport = function (elem) {
+
+	var bounding = elem.getBoundingClientRect();
+
+	var out = {};
+	out.right = bounding.right > (window.innerWidth || document.documentElement.clientWidth);
+
+	return out;
+
+};
