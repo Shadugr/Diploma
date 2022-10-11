8ee1a6a1fd6b4f5620e61d05a7218f08df5a654c
Shadugr <shadircomm@gmail.com>
2022-10-11 16:49:21 +0300

    add: admin menu and styles

---
+++ Pages/Shared/_Adminmenu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FMI_web.Pages.Shared
+{
+    public class _AdminmenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
